655c898db71af461f10a004df031b35ccea9377a
Maria T. Andersen <mteilmann93@gmail.com>
2020-10-31 14:33:08 +0100

    change height unit in metric unit system from meters to centimeters

--- BMI.js
+++ BMI.js
@@ -7,14 +7,14 @@ let shownWeightUnit = document.getElementById("selected_weight_unit");
 let shownHeightUnit = document.getElementById("selected_height_unit");
 
 shownWeightUnit.innerText = " kg";
-shownHeightUnit.innerText = " m";
+shownHeightUnit.innerText = " cm";
 
 select.addEventListener(
   "change",
   function () {
     if (select.selectedIndex === 0) {
       shownWeightUnit.innerText = " kg";
-      shownHeightUnit.innerText = " m";
+      shownHeightUnit.innerText = " cm";
     } else {
       shownWeightUnit.innerText = " lbs";
       shownHeightUnit.innerText = " in";
@@ -103,13 +103,13 @@ button.addEventListener("click", function () {
 /* The following function calculates a BMI value using either the metric unit system or the imperial unit system respectfully */
 
 function calculateBMI(input_height, input_weight, metric) {
-  if (metric) return input_weight / (input_height * input_height);
+  if (metric) return input_weight / ((input_height /100) * (input_height /100));
   return (703 * input_weight) / (input_height * input_height);
 }
 
 /* The following function calculates is also formed by the BMI calculation formula. However, in this case, WEIGHT is calculated depending on a BMi value and a given height.  */
 
 function marginWeight(input_height, bmiValue, metric) {
-  if (metric) return (bmiValue * (input_height * input_height)).toFixed(2);
+  if (metric) return (bmiValue * ((input_height/ 100) * (input_height/ 100))).toFixed(2);
   return ((bmiValue * (input_height * input_height)) / 703).toFixed(2);
 }
